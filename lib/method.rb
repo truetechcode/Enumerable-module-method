109091b1ca027dea0e34ac63a1a7b4c61e722b03
TERVER AOSU <terveraosu@gmail.com>
2019-04-17 12:57:57 +0100

    added rspec test to each Enumerable methods

--- lib/method.rb
+++ lib/method.rb
@@ -3,56 +3,43 @@ module Enumerable
   # my_each
   def my_each
     i = 0
+    result = []
     while i < size
-      if is_a?(Array) && block_given?
-        yield(self[i])
-      elsif block_given?
-        yield(to_a[i])
-      end
+      result << yield(self[i]) if block_given?
       i += 1
     end
-    self unless block_given?
+    return self unless block_given?
+    return result
   end
 
   # my_each_with_index
   def my_each_with_index
     i = 0
+    result = []
     while i < size
-      if is_a?(Array) && block_given?
-        yield(self[i], i)
-      elsif block_given?
-        yield(to_a[i], i)
-      end
+      result << yield(self[i], i) if block_given?
       i += 1
     end
-    self unless block_given?
+    return self unless block_given?
+    return result
   end
 
   # my_select
   def my_select
-    new_array = []
+    result = []
     i = 0
     while i < size
-      if is_a?(Array) && yield(self[i])
-        new_array.push(self[i])
-      elsif yield(self[i])
-        new_array.push(to_a[i])
-      end
+      result << self[i] if yield(self[i])
       i += 1
     end
-    new_array
+    result
   end
 
   # my_all?
   def my_all?
     i = 0
     while i < size
-      if is_a?(Array) && block_given?
-        return yield(self[i]) ? true : false
-      elsif block_given?
-        return yield(to_a[i]) ? true : false
-      end
-
+      return yield(self[i]) ? true : false
       i += 1
     end
     true
@@ -62,15 +49,10 @@ module Enumerable
   def my_none?
     i = 0
     while i < size
-      if is_a?(Array) && block_given?
-        return !yield(self[i]) ? true : false
-      elsif block_given?
-        return !yield(to_a[i]) ? true : false
-      end
-
+      return !self.include?(self[i]) if yield(self[i])
       i += 1
     end
-    false
+    true
   end
 
   # my_count
@@ -95,5 +77,3 @@ module Enumerable
     result
   end
 end
-
-# (1..10).to_a.my_none? {|i| i==10}
